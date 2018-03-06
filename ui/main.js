a3497f3d9bd2719f03b2ecbccef8bec823e9f70b
Ankit <ankit.email247@gmail.com>
2018-03-06 20:27:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,19 @@
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function(){
     counter = counter + 1;
-    var spa = document.getElementById('count');
-    spa.innerHTML = counter.toString();
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var counter = request.responseText;
+                var spa = document.getElementById('count');
+                spa.innerHTML = counter.toString();
+            }
+        }
+    };
+    
+    request.open('GET', 'http://ankitemail247.imad.hasura-app.io/counter',true);
+    request.send('null');
 };
