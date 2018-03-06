06d325df5419ca71e25223a3d0dc1aa3f3689d4d
Ankit <ankit.email247@gmail.com>
2018-03-06 22:27:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,12 +22,24 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    var names = ["name1","name2","name3","name4"];
-    var lists = '';
-    for(var i=0; i<names.length; i++){
-        lists += "<li>"+names[i]+"</li>";
-    }
+    var names = request.responseText;
+    names = JSON.parse(names);
+    var request = new XMLHttpRequest();
     
-    var ul = document.getElementById("name_list");
-    ul.innerHTML = lists;
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status===200){
+                var lists = '';
+                for(var i=0; i<names.length; i++){
+                lists += "<li>"+names[i]+"</li>";
+                }
+                
+                var ul = document.getElementById("name_list");
+                ul.innerHTML = lists;
+            }
+        }
+    };
+    
+    request.open('GET','http://ankitemail247.imad.hasura-app.io/request-name?name='+name, true);
+    request.send('null');
 };
