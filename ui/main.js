7fbff02f5db7b4611b1cea3cad38536d6ad0675d
Ankit <ankit.email247@gmail.com>
2018-03-06 19:59:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,5 @@
-console.log('Loaded!');
-
-//move image to left
-var img = document.getElementById('madi');
-var marginLeft =0;
-function moveRight(){
-    marginLeft += 1;
-    img.style.marginLeft = marginLeft+"px";
-}
-
-
-img.onclick = function() {
-    var interval = setInterval(moveRight,50);
+var button = document.getElementById('counter');
+button.onclick = function(){
+    counter = counter + 1;
+    button.innerHTML = counter;
 };
