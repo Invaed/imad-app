1807dff3a0112247038cade01b4327868a62360e
Ankit <ankit.email247@gmail.com>
2018-03-06 22:17:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,8 +92,8 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 var names = [];
-app.get('/request-name/:name',function(req, res){
-    var name = req.params.name;
+app.get('/request-name',function(req, res){
+    var name = req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
