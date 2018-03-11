acae77f6f7f3d9da87e792dac3a8d9b1bee1303f
Ankit <ankit.email247@gmail.com>
2018-03-11 20:10:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 
-var config={
-    user:'ankitemail247',
-    database:'ankitemail247',
-    host:'db.imad.hasura-app.io',
-    port:'5432',
-    password:process.env.DB_PASSWORD
+var config = {
+    user : 'ankitemail247',
+    database : 'ankitemail247',
+    host : 'db.imad.hasura-app.io',
+    port : '5432',
+    password : process.env.DB_PASSWORD
     
 };
 
@@ -99,18 +99,16 @@ app.get('/request-name',function(req, res){
     res.send(JSON.stringify(names));
 });
 
-var pool=new Pool(config);
-app.get('/test-db',function(req,res){
-    pool.query('SELECT * FROM test',function(err,result){
+var pool = new Pool(config);
+app.get("test-db", function(req, res){
+    pool.query('SELECT * FROM test',function(err, result){
         if(err){
-            res.status(500).send(err.toString());
+            return status(500).send(err.toString());
         }
         else{
             res.send(JSON.stringify(result));
         }
-        
     });
-    
 });
 
 app.get('/:articleName',function(req, res){
